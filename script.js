d1dbaee723b289ed8c9c8e3bf0e54ecc653f6b1c
jhenrycoates <jhenrycoates@gmail.com>
2024-09-05 15:56:11 -0500

    Add getHumanChoice to take the users input of Rock, Paper, or Scissors and moved global variables for getComputerChoice into getComputerChoice function.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-let x = Math.random() * 10;
-console.log(x);
-
-function getComputerChoice (x) {
+function getComputerChoice() {
+    let x = Math.random() * 10;
+    console.log(x);
     if (x <= 3.33 ) {
         return "Rock";
-    } else if (x > 3.33 && x < 6.66) {
+    } else if (x > 3.33 && x < 6.67) {
         return "Paper";
     } else {
         return "Scissors";
     }
 }
-console.log(getComputerChoice(x));
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let y = prompt("Choose between, Rock, Paper, or Scissors by typing in your selection.");
+    console.log(y)
+    if (y === "Rock") {
+        return "Rock";
+    } else if (y === "Paper") {
+        return "Paper";
+    } else if (y === "Scissors") {
+        return "Scissors";
+    } else {
+        return getHumanChoice();
+    }
+}
+
+console.log(getHumanChoice());
